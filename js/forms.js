5505319706406183f5eee75bb176cad311058e33
PattLD <118557101+PattLD@users.noreply.github.com>
2025-06-14 17:47:03 -0300

    finalmente validação

--- js/forms.js
+++ js/forms.js
@@ -1,25 +1,56 @@
 (() => {
     "use strict";
 
+    // VALIDAÇÃO PERSONALIZADA
+    function validarTelefone(input) {
+        if (!input) return;
+        const valorSemMascara = $(input).cleanVal(); //LIMPA A MÁSCARA
+        if (valorSemMascara) {
+            // VERIFICA SE CONTEM MAIS DE 11 DIGITOS (DDD + 9)
+            input.setCustomValidity(valorSemMascara.length < 11 ? 'Telefone incompleto.' : '');
+        }
+    }
+
+    // PERMITE ACRESCENTAR MAIS VALIDAÇÕES PERSONALIZADAS
+    function validarCampo(input) {
+        switch (input.id) {
+            case 'telefone':
+                validarTelefone(input);
+                break;
+        }
+    }
+
+    // ENCONTRA FORMULARIOS COM ESSA CLASSE 
     const forms = document.querySelectorAll(".needs-validation");
 
     Array.from(forms).forEach((form) => {
-        form.addEventListener(
-            "submit",
-            (event) => {
-                if (!form.checkValidity()) { //checa se o formulario é invalido (!)
-                    event.preventDefault(); // se invalido, previne envio
-                    event.stopPropagation(); //evita que o evento se propague para elementos pai
-                }
+        form.addEventListener("submit", (event) => {
+            r
+            form.querySelectorAll('input, select, textarea').forEach(validarCampo);
 
-                form.classList.add("was-validated"); //acrescenta a classe de verificação do bootstrap
-            },
-            false
-        );
-    });
-})();
+            // SE FORMULARIO NAO VALIDO:                                        
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
 
-// MASCARAS
+            // ACRESCENTA CLASSE DE VALIDAÇÃO DO BOOTSTRAP                                        
+            form.classList.add("was-validated");
+        }, false);
 
-$('#telefone').mask('(00) 00000-0000')
-$('#preco').mask("000.000,00", {reverse: true});
+        // VALIDAÇÃO PERSONALIZADA EM TEMPO REAL
+        form.addEventListener('input', (event) => {
+            const input = event.target; //ARMAZENA O IMPUT DO EVENTO
+
+            if (form.classList.contains('was-validated')) {
+                validarCampo(input); //RODA A FUNÇÃO COM O IMPUT SELECIONADO
+            }
+        })
+    }
+    )();
+
+})
+
+// MÁSCARAS 
+$('#telefone').mask('(00) 00000-0000');
+$('#preco').mask("000.000,00", { reverse: true });
